78d82808ed326cbf2b4ce08a65bab720d8442cee
Guilherme Augusto <guiidiasdc@gmail.com>
2025-03-23 19:27:01 -0300

    Ajustes no codigo

--- User.js
+++ User.js
@@ -1,9 +1,7 @@
 import chalk from "chalk";
-import crypto from "crypto";
 import * as utils from "./utils.js";
 import { json } from "stream/consumers";
 import fs from "fs/promises";
-import { log } from "console";
 
 const jsonPath = "./users.json";
 
@@ -22,14 +20,14 @@ export default class User {
       const users = await utils.readUsers(jsonPath);
       return users;
     } catch (err) {
-      throw err;
+      console.error(`Erro: `, err);
     }
   }
 
   static async criarUsuario(nome, email, nascimento, role, ativo = true, id) {
     try {
-      const users = await this.listarUsuarios();
       const novoUsuario = new User(nome, email, nascimento, role, ativo, id);
+      const users = await this.listarUsuarios();
       users.push(novoUsuario);
       await fs.writeFile(jsonPath, JSON.stringify(users, null, 2));
       console.log(`Usuario criado com sucesso `);
@@ -46,6 +44,7 @@ export default class User {
       console.error("Erro ao buscar usuário:", err);
     }
   }
+
   static async atualizarUsuario(id, novosDados) {
     try {
       let users = await User.listarUsuarios();
@@ -59,6 +58,7 @@ export default class User {
       console.error("Erro ao atualizar usuário:", err);
     }
   }
+
   static async deletarUsuarioPorID(id) {
     try {
       let users = await User.listarUsuarios();
@@ -74,15 +74,15 @@ export default class User {
 //* Teste de criar um usuario.
 //! Modifique os DADOS para testar.
 
-// await User.criarUsuario(
-//   "Guilherme Dias",
-//   "guiemail121@gmail.com",
-//   "15-12-2009",
-//   "Programador",
-//   true,
-//   4
-// );
-// console.log(`\nUsuario criado com sucesso\n`);
+await User.criarUsuario(
+  "Guilherme Dias - TESTE",
+  "guiemail121@gmail.com",
+  "15-12-2009",
+  "Programador",
+  true,
+  4
+);
+console.log(`\nUsuario criado com sucesso\n`);
 
 //* Teste de listar todos os usuarios.
 
@@ -95,7 +95,7 @@ export default class User {
 //* Teste de buscar usuario por ID.
 //! Modifique o ID para testar.
 
-// const id = 4;
+// const id = 2;
 // console.log(
 //   `Usuario com ID ${id} encontado. \n `,
 //   await User.buscarUsuarioPorID(id),
@@ -106,19 +106,19 @@ export default class User {
 //! Modifique os dados para alterar.
 //? O parametro ID é do usuario que será alterado.
 
-// const id = 4;
+// const id = 0;
 // await User.atualizarUsuario(id, {
 //   nome: "Liliana Dantas",
 //   email: "lilianaDantas@gmail.com",
 //   nascimento: "18-05-1995",
 //   role: "Assistente",
-//   ativo: true,
-//   id: 4,
+//   ativo: false,
+//   id: 0,
 // });
 
 //* Teste deletar usuario por ID
 //! Modifique os dados para alterar.
 //? O parametro ID é do usuario que será deletado.
 
-// const id = 4
+// const id = 0
 // await User.deletarUsuarioPorID(id)
